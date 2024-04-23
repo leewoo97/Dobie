60a03e04b2c043eb15573268eec452d34b7a893d
고은석 <ko2sist@gmail.com>
2024-04-23 13:14:40 +0900

    ADD: package directoies

---
+++ backend/src/main/java/com/dobie/backend/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.dobie.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Dobie API 명세서",
+                description = "Dobie 서비스 API 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+//                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+//                                .name("Authorization")
+//                                .type(SecurityScheme.Type.HTTP)
+//                                .scheme("Bearer")
+//                                .bearerFormat("JWT"))
+                );
+    }
+
+    // swagger 그룹 추가
+    // 전체보기
+//    @Bean
+//    public GroupedOpenApi all() {
+//        return GroupedOpenApi.builder()
+//                .group("a. 전체")
+//                .pathsToMatch("/api/v1/**")
+//                .build();
+//    }
+
+}
