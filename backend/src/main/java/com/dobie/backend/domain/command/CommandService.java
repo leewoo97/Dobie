1f3f87d17435815253f3c75aee4839e9d2d7da49
오건영 <o54711254@gmail.com>
2024-04-26 15:16:15 +0900

    FEAT : gitlab clone 메서드 구현

--- backend/src/main/java/com/dobie/backend/domain/command/CommandService.java
+++ backend/src/main/java/com/dobie/backend/domain/command/CommandService.java
@@ -6,9 +6,15 @@ public interface CommandService {
 
     void gitClone(String repositoryURL);
 
+    void gitCloneGitLab(String repositoryURL, String username, String password);
+
+    void gitCheckout(String path, String branchName);
+
     void gitPull(String path, String branchName);
 
     void build(String path, String projectName);
 
     void run(String path, int port1, int port2);
+
+    void dockerComposeDown();
 }
--- backend/src/main/java/com/dobie/backend/domain/command/CommandServiceImpl.java
+++ backend/src/main/java/com/dobie/backend/domain/command/CommandServiceImpl.java
@@ -52,6 +52,46 @@ public class CommandServiceImpl implements CommandService {
         }
     }
 
+    @Override
+    public void gitCloneGitLab(String repositoryURL, String username, String password) {
+        StringBuilder sb = new StringBuilder();
+        // URL에 사용자 이름과 액세스 토큰을 포함
+        String authUrl = repositoryURL.replace("https://", "https://" + username + ":" + password + "@");
+        sb.append("git clone ").append(authUrl);
+        CommandLine commandLine = CommandLine.parse(sb.toString());
+
+        DefaultExecutor executor = new DefaultExecutor();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+        executor.setStreamHandler(streamHandler);
+
+        try {
+            executor.execute(commandLine);
+            String result = outputStream.toString().trim(); // 명령어 실행 결과를 문자열로 받음
+            System.out.println("git clone 성공: " + result);
+        } catch (Exception e) {
+            System.out.println("git clone 명령어 실행 중 에러 발생: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void gitCheckout(String path, String branchName) {
+        sb = new StringBuilder();
+        sb.append("git -C ").append(path).append(" checkout ").append(branchName);
+        CommandLine commandLine = CommandLine.parse(sb.toString());
+        executor.setStreamHandler(streamHandler);
+        try {
+            executor.execute(commandLine);
+            String result = outputStream.toString().trim();
+            System.out.println("git checkout 성공: " + result);
+        } catch (Exception e) {
+            System.out.println("git checkout 명령어 실행 중 에러 발생: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     // git pull
     @Override
     public void gitPull(String path, String branchName) {
@@ -74,7 +114,7 @@ public class CommandServiceImpl implements CommandService {
     @Override
     public void build(String path, String projectName) {
         sb = new StringBuilder();
-        sb.append("docker build -t ").append(projectName + " .");
+        sb.append("docker build ").append(projectName + " .");
 
         CommandLine commandLine = CommandLine.parse(sb.toString());
         executor.setStreamHandler(streamHandler);
@@ -103,6 +143,22 @@ public class CommandServiceImpl implements CommandService {
             System.out.println("실행 성공: " + result);
         } catch (Exception e) {
             System.out.println("실행 중 에러 발생: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dockerComposeDown() {
+        sb = new StringBuilder();
+        sb.append("docker compose down");
+        CommandLine commandLine = CommandLine.parse(sb.toString());
+        executor.setStreamHandler(streamHandler);
+        try {
+            executor.execute(commandLine);
+            String result = outputStream.toString().trim(); // 명령어 실행 결과를 문자열로 받음
+            System.out.println("compose down 성공: " + result);
+        } catch (Exception e) {
+            System.out.println("compose down 중 에러 발생: " + e.getMessage());
             e.printStackTrace();
         }
     }
