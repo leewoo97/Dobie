d36faed599557bd7711b834b5ebe12860f0b42f4
유호정 <ys0403ab@naver.com>
2024-04-29 11:04:14 +0900

    FIX: config 파일 틀 수정, proxy pass 코드 수정

--- backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigServiceImpl.java
+++ backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigServiceImpl.java
@@ -8,24 +8,6 @@ public class NginxConfigServiceImpl implements NginxConfigService{
     @Override
     public String createReverseProxyNginxConfig(NginxConfigGetResponseDto nginxConfig) {
         StringBuilder sb = new StringBuilder();
-        sb.append("user nginx;\n");
-        sb.append("worker_processes auto;\n");
-        sb.append("\n");
-        sb.append("error_log /var/log/nginx/error.log warn;\n");
-        sb.append("pid /run/nginx.pid;\n");
-        sb.append("\n");
-        sb.append("events {\n");
-        sb.append("    worker_connections 1024;\n");
-        sb.append("}\n");
-        sb.append("http {\n");
-        sb.append("    include /etc/nginx/mime.types;\n");
-        sb.append("    default_type application/octet-stream;\n");
-        sb.append("    log_format main '$remote_addr - $remote_user [$time_local] \"$request\" '\n");
-        sb.append("                    '$status $body_bytes_sent \"$http_referer\" '\n");
-        sb.append("                    '\"$http_user_agent\" \"$http_x_forwarded_for\"'\n");
-        sb.append("    access_log /var/log/nginx/access.log main;\n");
-        sb.append("    sendfile on;\n");
-        sb.append("    keepalive_timeout 65;\n");
 
         if(nginxConfig.isHttps()){
             sb.append(httpsConfig(nginxConfig));
@@ -33,31 +15,30 @@ public class NginxConfigServiceImpl implements NginxConfigService{
             sb.append(defaultConfig(nginxConfig));
         }
 
-
-
-
         return sb.toString();
     }
+    @Override
     public String httpsConfig(NginxConfigGetResponseDto nginxConfig){
         StringBuilder sb = new StringBuilder();
         sb.append("server {\n");
-        sb.append("    listen 80;\n");
-        sb.append("    server_name "+nginxConfig.getDomain()+"\n"); //도메인 없을경우 수정
+        sb.append("    listen 80;\n"); //80번 포트로 요청이 들어왔을때
+        sb.append("    server_name "+nginxConfig.getDomain()+"\n");
         sb.append("    location / {\n");
-        sb.append("        return 301 https://$host$request_url;\n");
+        sb.append("        return 301 https://$host$request_url;\n"); //443포트로 보내기(https적용할 수 있도록)
         sb.append("   }\n");
         sb.append("}\n");
         sb.append("server {\n");
-        sb.append("    listen 443 ssl\n");
-        sb.append("    server_name "+nginxConfig.getDomain()+"\n"); //도메인 없을경우 수정
+        sb.append("    listen 443 ssl\n"); //443포트로 요청이 들어왔을때 (ssl은 https의 보안기능을 사용한다~~ 이런 느낌)
+        sb.append("    server_name "+nginxConfig.getDomain()+"\n"); //사용할 서버네입
         sb.append("\n");
-        sb.append("    ssl_certificate "+nginxConfig.getSslCertificate()+"\n");
-        sb.append("    ssl_certificate_key "+nginxConfig.getSslCertificateKey()+"\n");
+        sb.append("    ssl_certificate "+nginxConfig.getSslCertificate()+"\n"); //직접 받은 ssl certificate 넣기(우리가 받아주는 방법 찾아보기)
+        sb.append("    ssl_certificate_key "+nginxConfig.getSslCertificateKey()+"\n"); //직접 받은 ssl certificate key 넣기
         //보안 강화 추가
         sb.append("\n");
+        //locations 리스트 for문 통해서 config 작성
         for(NginxProxyDto proxyConfig : nginxConfig.getLocations()){
-            sb.append("    location ").append(proxyConfig.getLocation()).append(" {\n");
-            sb.append("        proxy_pass ").append(proxyConfig.getProxyPass()).append(";\n");
+            sb.append("    location ").append(proxyConfig.getLocation()).append(" {\n"); //location 경로 (ex. location /api {)
+            sb.append("        proxy_pass ").append(proxyConfig.getServiceName()).append(":").append(proxyConfig.getPort()).append(";\n"); //위의 경로로 요청이 왔을때 연결할 경로(ex. https://www.catale.8080)
             sb.append("        proxy_http_version 1.1;\n");
             sb.append("        proxy_set_header Connection \"\";\n");
             sb.append("        proxy_redirect off;\n");
@@ -76,6 +57,7 @@ public class NginxConfigServiceImpl implements NginxConfigService{
 
         return sb.toString();
     }
+    @Override
     public String defaultConfig(NginxConfigGetResponseDto nginxConfig){
         StringBuilder sb = new StringBuilder();
         sb.append("server {\n");
@@ -85,7 +67,7 @@ public class NginxConfigServiceImpl implements NginxConfigService{
         sb.append("        return 301 https://$host$request_url;\n");
         for(NginxProxyDto proxyConfig : nginxConfig.getLocations()){
             sb.append("    location ").append(proxyConfig.getLocation()).append(" {\n");
-            sb.append("        proxy_pass ").append(proxyConfig.getProxyPass()).append(";\n");
+            sb.append("        proxy_pass ").append(proxyConfig.getServiceName()).append(":").append(proxyConfig.getPort()).append(";\n");
             sb.append("        proxy_http_version 1.1;\n");
             sb.append("        proxy_set_header Connection \"\";\n");
             sb.append("        proxy_redirect off;\n");
