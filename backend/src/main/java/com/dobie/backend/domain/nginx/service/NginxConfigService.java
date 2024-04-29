364ddf68fe424403fc2ea70c7776b56c43517aa8
유호정 <ys0403ab@naver.com>
2024-04-29 11:03:13 +0900

    FIX: 메서드 추가

--- backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigService.java
+++ backend/src/main/java/com/dobie/backend/domain/nginx/service/NginxConfigService.java
@@ -4,4 +4,8 @@ import com.dobie.backend.domain.nginx.dto.NginxConfigGetResponseDto;
 
 public interface NginxConfigService {
     String createReverseProxyNginxConfig(NginxConfigGetResponseDto nginxConfig);
+
+    String httpsConfig(NginxConfigGetResponseDto nginxConfig);
+
+    String defaultConfig(NginxConfigGetResponseDto nginxConfig);
 }
