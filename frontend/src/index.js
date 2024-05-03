f792a85311953be1fa52aebde4122b297b1ee604
고은석 <ko2sist@gmail.com>
2024-05-03 11:17:58 +0900

    REMOVE: redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+
+    <App />
+
 );
 
 // If you want to start measuring performance in your app, pass a function
