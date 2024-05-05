d382a343e84d537e29f860e9e1e8ca6a2619ee4d
박헤선 <qkr8364@naver.com>
2024-05-06 03:51:54 +0900

    FEAT: LoginPage css 완료

--- frontend/src/pages/startpage/LoginPage.jsx
+++ frontend/src/pages/startpage/LoginPage.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Container from "../../components/common/Container";
+import styles from "./LoginPage.module.css";
+import mascot from "../../assets/mascot.png";
 
 export default function LoginPage() {
 
-    return(
+    return (
         <Container>
-            <p>로그인</p>
+            <div className={styles.content}>
+                <div className={styles.title}>
+                    <img src={mascot} alt="" width="120px" decoding="async" />
+                    <div className={styles.dobie}>Dobie</div>
+                </div>
+                <input type="text" placeholder="username"></input>
+                <input type="password" placeholder="password"></input>
+                <div className={styles.button}>
+                    <div className={styles.login}>로그인</div>
+                </div>
+            </div>
         </Container>
     );
 }
