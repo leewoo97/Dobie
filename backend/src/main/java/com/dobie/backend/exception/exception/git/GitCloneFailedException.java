c1dfbbc09e74592a8fc711cccfb86b89137f7961
고은석 <ko2sist@gmail.com>
2024-05-05 19:53:54 +0900

    REFACTOR: gitCloneFailedException

--- backend/src/main/java/com/dobie/backend/exception/exception/git/GitCloneFailedException.java
+++ backend/src/main/java/com/dobie/backend/exception/exception/git/GitCloneFailedException.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 @Getter
 public class GitCloneFailedException extends RuntimeException {
     private final ErrorCode errorCode;
+    private final String errorMessage;
 
-    public GitCloneFailedException(){
+    public GitCloneFailedException(String errorMessage){
         this.errorCode = ErrorCode.GIT_CLONE_FAILED;
+        this.errorMessage = errorMessage;
     }
 }
--- backend/src/main/java/com/dobie/backend/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/dobie/backend/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dobie.backend.exception.handler;
 import com.dobie.backend.exception.exception.Environment.BuildGradleNotFoundException;
 import com.dobie.backend.exception.exception.Environment.FilePathNotExistException;
 import com.dobie.backend.exception.exception.Environment.PomXmlNotFoundException;
+import com.dobie.backend.exception.exception.git.GitCloneFailedException;
 import com.dobie.backend.exception.format.code.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -33,6 +34,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(FilePathNotExistException.class)
     protected ResponseEntity<?> handle(FilePathNotExistException e) {
         log.error("FilePathNotExistException = {}", e.getErrorCode().getMessage());
+        return response.error(e.getErrorCode());
+    }
+
+
+    @ExceptionHandler(GitCloneFailedException.class)
+    protected ResponseEntity<?> handle(GitCloneFailedException e) {
+        log.error("GitCloneFailedException = {}", e.getErrorCode().getMessage());
+        log.error("Error Message = {}", e.getErrorMessage());
         return response.error(e.getErrorCode());
     }
 }
