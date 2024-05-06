569d8dba5a0461245f708e2d558e349d00bb627e
유호정 <ys0403ab@naver.com>
2024-05-06 17:27:50 +0900

    FIX: Welcome 페이지 css 수정

--- frontend/src/pages/startpage/WelcomePage.jsx
+++ frontend/src/pages/startpage/WelcomePage.jsx
@@ -3,42 +3,40 @@ import Container from "../../components/common/Container";
 import mascot from "../../assets/mascot.png";
 import styles from "./WelcomePage.module.css";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function WelcomePage() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // useEffect(() => {
+  //     const timer = setTimeout(() => {
+  //         navigate('/login');  // "/login"은 LoginPage 경로입니다.
+  //     }, 2000);  // 2000 밀리초 후에 실행
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            navigate('/login');  // "/login"은 LoginPage 경로입니다.
-        }, 2000);  // 2000 밀리초 후에 실행
+  //     // 컴포넌트 언마운트 시 타이머 클리어
+  //     return () => clearTimeout(timer);
+  // }, [navigate]);  // navigate가 변경될 때마다 useEffect를 다시 실행하지 않도록
 
-        // 컴포넌트 언마운트 시 타이머 클리어
-        return () => clearTimeout(timer);
-    }, [navigate]);  // navigate가 변경될 때마다 useEffect를 다시 실행하지 않도록
-
-
-    return (
-        <Container>
-            <div className={styles.content}>
-                <div className={styles.mascot}>
-                    <img src={mascot} alt="" width="160px" decoding="async" />
-                </div>
-                <div>
-                    <div className={styles.dobie}>
-                        Dobie
-                    </div>
-                    <div className={styles.eng}>
-                        <div className={styles.line1}>Docker newbie</div>
-                        <div className={styles.line2}>Docker is free</div>
-                    </div>
-                    <div className={styles.kor}>
-                        <div className={styles.line3}>인프라로부터</div>
-                        <div className={styles.line4}>자유로워지자</div>
-                    </div>
-                </div>
+  return (
+    <Container>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.mascot}>
+            <img src={mascot} alt="" width="160px" decoding="async" />
+          </div>
+          <div>
+            <div className={styles.dobie}>Dobie</div>
+            <div className={styles.eng}>
+              <div className={styles.line1}>Docker newbie</div>
+              <div className={styles.line2}>Docker is free</div>
             </div>
-        </Container>
-    );
+            <div className={styles.kor}>
+              <div className={styles.line3}>인프라로부터</div>
+              <div className={styles.line4}>자유로워지자</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
