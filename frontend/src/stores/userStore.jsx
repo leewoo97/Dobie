c098ef6ce3c749c3b97821ea8f1c9eb5986056ab
유호정 <ys0403ab@naver.com>
2024-05-07 21:53:47 +0900

    FIX: 줄 정렬

--- frontend/src/stores/userStore.jsx
+++ frontend/src/stores/userStore.jsx
@@ -1,20 +1,22 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create(persist(    
-    set => ({
-        user: {
-            username: "",
-            password: "",
-        },
-        setUser: (user) => {
-            set({user : user});
-        },
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: {
+        username: "",
+        password: "",
+      },
+      setUser: (user) => {
+        set({ user: user });
+      },
     }),
     {
-        name: "user-storage", // 저장될 localStorage의 key 이름
-        getStorage: () => localStorage // 사용할 storage를 명시적으로 지정
+      name: "user-storage", // 저장될 localStorage의 key 이름
+      getStorage: () => localStorage, // 사용할 storage를 명시적으로 지정
     }
-))
+  )
+);
 
 export default useUserStore;
