9d78be04ec3e7838bdc4bad6acf285a1933d6951
유호정 <ys0403ab@naver.com>
2024-05-07 16:07:30 +0900

    FIX: 가이드 페이지 css 수정

--- frontend/src/pages/mainpage/GuidePage.jsx
+++ frontend/src/pages/mainpage/GuidePage.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Container from "../../components/common/Container";
+import styles from "./GuidePage.module.css";
+
 import NavTop from "../../components/common/NavTop";
 
-export default function GuidePage(){
-    return(
-        <Container>
-            <NavTop/>
-        </Container>
-    );
+export default function GuidePage() {
+  return (
+    <>
+      <NavTop />
+      <div className={styles.page}>
+        <h1>가이드 페이지</h1>
+      </div>
+    </>
+  );
 }
