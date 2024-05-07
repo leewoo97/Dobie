c9e446e9812722b7a11a74274163927592c49993
유호정 <ys0403ab@naver.com>
2024-05-08 01:02:03 +0900

    FIX: Main.jsx -> Project.jsx axios 이동

--- frontend/src/api/Project.jsx
+++ frontend/src/api/Project.jsx
@@ -11,3 +11,13 @@ export async function getProject() {
     throw error;
   }
 }
+//프로젝트 삭제
+export async function deleteProject(projectId) {
+  try {
+    const response = await axios.delete(`${projectUrl}/delete/${projectId}`);
+    return response;
+  } catch (error) {
+    // console.log("회원가입 실패: " + error);
+    throw error;
+  }
+}
