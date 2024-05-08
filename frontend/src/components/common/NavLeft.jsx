1279ad505fb7a0ecb06e75578e4edeeee4de8b27
고은석 <ko2sist@gmail.com>
2024-05-08 16:53:41 +0900

    FEAT: left navbar bold

--- frontend/src/components/common/NavLeft.jsx
+++ frontend/src/components/common/NavLeft.jsx
@@ -3,11 +3,15 @@ import styles from "./NavLeft.module.css";
 import s from "classnames";
 import mainBtn from "../../assets/btn_main.png";
 import usePrjectStore from "../../stores/projectStore";
+import { useParams } from "react-router-dom";
 
 export default function NavLeft({ num }){
     const navigate = useNavigate();
     const { selectedProject, setSelectedProject } = usePrjectStore();
 
+    const params = useParams();
+    const serviceId = params.serviceId;
+    const databaseId = params.databaseId;
 
 
     return(
@@ -19,7 +23,7 @@ export default function NavLeft({ num }){
                 <p className={num === 3 ? styles.text2 : styles.text} >Backend</p>
                 {/* 여기는 BackendMap */}
                 {Object.values(selectedProject.backendMap).map((backendProject) => (
-                    <div className={styles.text3} key={backendProject.serviceId} onClick={()=> navigate(`/manage/backend/${backendProject.serviceId}`) }>{backendProject.framework}</div>
+                    <div className={backendProject.serviceId === serviceId ? styles.text3 : ""} key={backendProject.serviceId} onClick={()=> navigate(`/manage/backend/${backendProject.serviceId}`) }>{backendProject.framework}</div>
                 ))}
                 </div>
                 <p className={num === 4 ? styles.text2 : styles.text} onClick={() => navigate("/manage/frontend")}>Frontend</p>
@@ -27,7 +31,7 @@ export default function NavLeft({ num }){
                 <p className={num === 5 ? styles.text2 : styles.text} >DB</p>
                 {/* 여기는 DatabaseMap */}
                 {Object.values(selectedProject.databaseMap).map((databaseProject) => (
-                    <div className={styles.text3} key={databaseProject.databaseId} onClick={() => navigate(`/manage/database/${databaseProject.databaseId}`)}>{databaseProject.databaseType}</div>
+                    <div className={databaseProject.databaseId === databaseId ? styles.text3 : ""} key={databaseProject.databaseId} onClick={() => navigate(`/manage/database/${databaseProject.databaseId}`)}>{databaseProject.databaseType}</div>
                 ))}
                 </div>
             </div>
