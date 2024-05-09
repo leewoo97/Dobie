b3a6abe3d658f53fedb80a8f3d6f02f693298df6
Sist <ko2sist@gmail.com>
2024-05-10 08:15:11 +0900

    FIX: CreateBackendPage

--- frontend/src/pages/createPage/CreateBackendPage.jsx
+++ frontend/src/pages/createPage/CreateBackendPage.jsx
@@ -1,12 +1,23 @@
 import NavTop from "../../components/common/NavTop";
 import NavLeftCreate from "../../components/common/NavLeftCreate";
 import BackendFrame from "../../components/create/BackendFrame";
+import useProjectStore from "../../stores/projectStore";
 
 export default function CreateBackendPage() {
+  const {newProject} = useProjectStore();
+
+  console.log(newProject.backendMap);
+
   return (
     <>
       <NavTop />
       <NavLeftCreate num={3} />
+      <div>
+        {Object.values(newProject.backendMap).map((backend, index) => {
+          return <div key={index}>test</div>
+
+        })}
+      </div>
       <BackendFrame />
     </>
   );
