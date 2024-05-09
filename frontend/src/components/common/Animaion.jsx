377dd18e680157c2de8b0acc934875dcf623c7cc
박헤선 <qkr8364@naver.com>
2024-05-09 09:42:27 +0900

    FEAT: 로딩애니메이션추가

---
+++ frontend/src/components/common/Animaion.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+
+import animation from "../../assets/loding.json";
+
+export default function LottieAnimation() {
+    const animationContainer = useRef(null);
+  
+    useEffect(() => {
+      const anim = lottie.loadAnimation({
+        container: animationContainer.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: animation,
+      });
+  
+      return () => anim.destroy();
+    }, [animation]);
+  
+    return <div ref={animationContainer}></div>;
+  }
