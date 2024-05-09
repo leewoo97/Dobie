b8e06e41fc870a47fa399c7abb32e4e943b34846
고은석 <ko2sist@gmail.com>
2024-05-09 17:15:47 +0900

    FIX: ProjectTopCreate component

--- frontend/src/components/common/ProjectTopCreate.jsx
+++ frontend/src/components/common/ProjectTopCreate.jsx
@@ -3,14 +3,13 @@ import styles from "./ProjectTopCreate.module.css";
 import remove from "../../assets/deleteIcon.png";
 import add from "../../assets/createIcon.png"
 import useProjectStore from "../../stores/projectStore";
+import { createProject } from "../../api/Project";
 
-export default function GetBox({ keyName, valueName }) {
-    const {newProject, projectMap, setProjectMap} = useProjectStore();
-    // const {tempProjectMap, setTempProjectMap} = useState(new Map({...projectMap}));
-    console.log(projectMap);
-    // console.log(new Map(Object.entries({...projectMap})));
-    const createProject = () => {
-        
+export default function ProjectTopCreate({ keyName, valueName }) {
+    const {newProject} = useProjectStore();
+    
+    const createProjectHandler = () => {
+        createProject(newProject);
     }
 
     return (
@@ -20,7 +19,7 @@ export default function GetBox({ keyName, valueName }) {
                 <div className={styles.text}>Git에 등록한 Project명 또는 Repository명을 등록하세요 <br></br>( = 최상단 폴더 이름)</div>
             </div>
             <div className={styles.buttons}>
-                <div className={styles.add}>등록 <img src={add} alt="" width="23px" decoding="async" className={styles.btnIcon} /></div>
+                <div className={styles.add}>등록 <img src={add} alt="" width="23px" decoding="async" className={styles.btnIcon} onClick={createProjectHandler}/></div>
             </div>
         </div>
     );
