87f3a3458981807e33a7b39312f5c2dfc65c7f42
박헤선 <qkr8364@naver.com>
2024-05-10 09:54:10 +0900

    FEAT: create css 수정 중

--- frontend/src/components/common/NavLeftCreate.jsx
+++ frontend/src/components/common/NavLeftCreate.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import styles from "./NavLeftCreate.module.css";
 import s from "classnames";
 import mainBtn from "../../assets/btn_main.png";
+import add from "../../assets/createIcon.png";
+import home from "../../assets/homeIcon.png";
 
 export default function NavLeftCreate({ num }){
     const navigate = useNavigate();
@@ -13,7 +15,11 @@ export default function NavLeftCreate({ num }){
                 <p className={num === 4 ? styles.text2 : styles.text} onClick={() => navigate("/create/frontend")}>Frontend</p>
                 <p className={num === 5 ? styles.text2 : styles.text} onClick={() => navigate("/create/database")}>DB</p>
             </div>
-            <img src={mainBtn} alt="search_icon" onClick={() => navigate("/main")} className={styles.img}/>
+            <div className={styles.buttons}>
+                <div className={styles.add}>프로젝트 등록 <img src={add} alt="" decoding="async" className={styles.btnIcon} /></div>
+                <div className={styles.home}>메인페이지 <img src={home} alt="" decoding="async" className={styles.btnIcon} /></div>
+                {/* <img src={mainBtn} alt="search_icon" onClick={() => navigate("/main")} className={styles.img}/> */}
+            </div>
         </div>
     );
 
