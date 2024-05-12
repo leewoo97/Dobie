2904e18702ba79150ec6cbef19dee45c1613ab53
심규리 <jinry0034@naver.com>
2024-05-13 02:26:24 +0900

    FEAT:수정 페이지 버튼 설정

--- frontend/src/components/update/BackendFrame.jsx
+++ frontend/src/components/update/BackendFrame.jsx
@@ -3,18 +3,47 @@ import styles from "./BackendFrame.module.css";
 import InputBox from "../common/InputBox";
 import InputSelectBox from "../common/InputSelectBox";
 import DescBox from "../common/DescBox";
-import ProjectTopUpdate from "../common/ProjectTopUpdate";
+import {v4 as uuidv4} from "uuid";
 import useProjectStore from "../../stores/projectStore";
 
-export default function BackendFrame({serviceId}) {
+export default function BackendFrame() {
 
     const {updatedProject, setUpdatedProject} = useProjectStore();
-    const [tempProject, setTempProject] = useState({
-        ...updatedProject,
-        backendMap: {
-            ...updatedProject.backendMap,
-            [serviceId]: {...updatedProject.backendMap[serviceId]}
-        }});
+    const [tempProject, setTempProject] = useState({...updatedProject,});
+    const [selectedKey, setSelectedKey] = useState(Object.keys(tempProject.backendMap).at(0));
+    const [selectedBackend, setSelectedBackend] = useState({...tempProject.backendMap[selectedKey]});
+
+    console.log(selectedBackend);
+    // console.log(tempProject);
+    const clickKeyHandler = (key) => {
+        setSelectedKey(key);
+    }
+    const clickDeleteHandler = (key) => {
+        const removeKeyIndex = Object.keys(tempProject.backendMap).indexOf(key);
+        const moveKeyIndex = removeKeyIndex === 0 ? 0 : removeKeyIndex-1;
+        delete tempProject.backendMap[key];
+        setSelectedKey(Object.keys(tempProject.backendMap).at(moveKeyIndex));
+        setTempProject({...tempProject});
+    }
+
+    const emptyBackend = {
+        serviceName: "",
+        language: "",
+        version: "",
+        framework: "",
+        path: "",
+        branch: "",
+        location: "",
+        externalPort: "",
+        internalPort: ""
+    };
+
+    const addEmptyBackend = () => {
+        const newKey = uuidv4();
+        tempProject.backendMap[newKey] = emptyBackend;
+        setSelectedKey(newKey);
+        setTempProject({...tempProject});
+    }
 
     const frameworkList = [
         "SpringBoot(gradle)",
@@ -34,60 +63,55 @@ export default function BackendFrame({serviceId}) {
 
     const frameworkSelect = (value) => {
         setFramework(value);
-        console.log(value);
     };
 
     const versionSelect = (value) => {
         setVersion(value);
-        console.log(value);
     };
 
     const changePathHandler = (e) => {
-        setTempProject(prev => ({
-            ...prev,
-            backendMap: {
-                ...prev.backendMap,
-                [serviceId]: {
-                    ...prev.backendMap[serviceId],
-                    path: e.target.value
-                }
-            }
-        }))
+       selectedBackend.path = e.target.value;
+       tempProject.backendMap[selectedKey] = {...selectedBackend};//??
+       setTempProject({...tempProject}); //??
     }
 
     const changeInternalPortHandler = (e) => {
-        setTempProject(prev => ({
-            ...prev,
-            backendMap : {
-                ...prev.backendMap,
-                [serviceId]: {
-                    ...prev.backendMap[serviceId],
-                    internalPort : Number(e.target.value)
-                }
-            }
-        }))
+        selectedBackend.internalPort = Number(e.target.value);
+        tempProject.backendMap[selectedKey] = {...selectedBackend};
+        setTempProject({...tempProject});
     }
     // 컴포넌트가 업데이트 될 때마다 상위 스토어의 상태를 업데이트
     useEffect(()=>{
         setUpdatedProject(tempProject);
-    },[tempProject])
+    },[tempProject]);
+
+    useEffect(() => {
+        setSelectedBackend({...tempProject.backendMap[selectedKey]});
+    },[selectedKey]);
 
     return (
         <div className={styles.page}>
-            {/* <ProjectTopUpdate updatedProject={updatedProject} /> */}
+            {Object.keys(tempProject.backendMap).map((key, index) => {
+                return <div key={index}>
+                    <button onClick={()=>clickKeyHandler(key)}>{index+1}</button>
+                    <button onClick={()=>clickDeleteHandler(key)}> x </button>
+                </div>
+            })}
+            <button onClick={addEmptyBackend}> + </button>
+
             <InputSelectBox keyName={"프레임워크"} list={frameworkList} value={framework} onChange={frameworkSelect} />
             <DescBox desc={"Backend 서비스의 프레임워크를 선택하세요"} />
             
             <InputSelectBox keyName={"언어버전"} list={versionList} value={version} onChange={versionSelect} />
             <DescBox desc={"Backend 서비스의 언어 버전을 선택하세요"} />
             
-            <InputBox keyName={"폴더 경로"} value={tempProject.backendMap[serviceId].path} valueName={"/frontend"} onChange={changePathHandler} />
+            <InputBox keyName={"폴더 경로"} value={selectedBackend.path} valueName={"/frontend"} onChange={changePathHandler} />
             <DescBox desc={"프로젝트 루트 경로로부터 해당 프레임워크 폴더 경로를 작성하세요"} />
             
             {/* <InputBox keyName={"브랜치"} valueName={"dev-be"} />
             <DescBox desc={"해당 프레임워크를 빌드 시킬 브랜치명을 작성하세요"} /> */}
             
-            <InputBox keyName={"내부 포트 번호"} value={tempProject.backendMap[serviceId].internalPort} valueName={"8080"}onChange={changeInternalPortHandler}/>
+            <InputBox keyName={"내부 포트 번호"} value={selectedBackend.internalPort} valueName={"8080"}onChange={changeInternalPortHandler}/>
             <DescBox desc={"해당 프레임워크가 사용할 포트 번호를 지정해주세요"} />
 
             {/* <InputBox keyName={"외부 포트 번호"} valueName={"8080"} />
--- frontend/src/pages/updatePage/UpdateDatabasePage.jsx
+++ frontend/src/pages/updatePage/UpdateDatabasePage.jsx
@@ -6,33 +6,11 @@ import useProjectStore from "../../stores/projectStore";
 
 export default function UpdateDatabasePage() {
 
-  const {updatedProject} = useProjectStore();
-  const [selectedDatabaseId, setSelectedDatabseId] = useState( null );
-
-  useEffect(() => {
-    const firstDatabaseId = Object.keys(updatedProject.databaseMap)[0];
-    setSelectedDatabseId(firstDatabaseId);
-  }, [updatedProject.databaseMap]);
-
-  const handleSelectDatabaseId = (databaseId) => {
-    setSelectedDatabseId(databaseId);
-  }
-  console.log(selectedDatabaseId);
-
   return (
     <>
-      {/* <NavTop /> */}
+      <NavTop />
       <NavLeftUpdate num={5} />
-      <div>
-        {Object.entries(updatedProject.databaseMap).map((database, index)=> (
-          <button key={database.at(0)} onClick={()=> handleSelectDatabaseId(database.at(0))}>
-            {index+1}
-          </button>
-        ))}
-        {selectedDatabaseId && (
-          <DatabaseFrame databaseId={selectedDatabaseId}/>
-        )}
-      </div>
+      <DatabaseFrame/>
     </>
   );
 }
