e6a906570cfdcebaceb8dd69e9fd91e9fb54b962
고은석 <ko2sist@gmail.com>
2024-05-13 09:55:18 +0900

    FEAT: tab css

--- frontend/src/components/create/BackendFrame.jsx
+++ frontend/src/components/create/BackendFrame.jsx
@@ -86,75 +86,67 @@ export default function BackendFrame() {
 
   const versionList = ["java 8", "java 11", "java 17", "python 3.10"];
 
-  const [framework, setFramework] = useState("");
-  const [version, setVersion] = useState("");
-
-  const frameworkSelect = (value) => {
-    setFramework(value);
-    console.log(value);
-  };
-
-  const versionSelect = (value) => {
-    setVersion(value);
-    console.log(value);
-  };
-
   return (
     <div className={styles.page}>
       <div className={styles.tapBox}>
         {Object.keys(tempProject.backendMap).map((key, index) => {
           return (
             <div key={index} className={key === selectedKey ? styles.selectedTap : styles.tap}>
-              <button onClick={() => clickKeyHandler(key)}>{index + 1}</button>
-              <button onClick={() => clickDeleteHandler(key)}> x </button>
+              <p onClick={() => clickKeyHandler(key)}>{index + 1}</p>
+              <p onClick={() => clickDeleteHandler(key)}> x </p>
             </div>
           );
         })}
-        <button onClick={addEmptyBackend}>+</button>
+        <p onClick={addEmptyBackend} className={styles.tapPlus}>+</p>
       </div>
-      
 
-      {/* <div>{selectedKey}</div> */}
+      <div className={Object.keys(tempProject.backendMap).length !== 0 && styles.hide}>
+        test
+      </div>
 
-      <InputSelectBox
-        keyName={"프레임워크"}
-        list={frameworkList}
-        value={selectedBackend.framework}
-        onChange={changeFrameworkHandler}
-      />
-      <DescBox desc={"Backend 서비스의 프레임워크를 선택하세요"} />
+      <div className={Object.keys(tempProject.backendMap).length === 0 && styles.hide}>
+        {/* <div>{selectedKey}</div> */}
 
-      <InputSelectBox
-        keyName={"언어버전"}
-        list={versionList}
-        value={version}
-        onChange={changeVersionHandler}
-      />
-      <DescBox desc={"Backend 서비스의 언어 버전을 선택하세요"} />
+        <InputSelectBox
+          keyName={"프레임워크"}
+          list={frameworkList}
+          value={selectedBackend.framework}
+          onChange={changeFrameworkHandler}
+        />
+        <DescBox desc={"Backend 서비스의 프레임워크를 선택하세요"} />
 
-      <InputBox
-        keyName={"폴더 경로"}
-        valueName={"/backend"}
-        value={selectedBackend.path}
-        onChange={changePathHandler}
-      />
-      <DescBox
-        desc={"프로젝트 루트 경로로부터 해당 프레임워크 폴더 경로를 작성하세요"}
-      />
+        <InputSelectBox
+          keyName={"언어버전"}
+          list={versionList}
+          value={selectedBackend.language+" "+selectedBackend.version}
+          onChange={changeVersionHandler}
+        />
+        <DescBox desc={"Backend 서비스의 언어 버전을 선택하세요"} />
 
-      {/* <InputBox keyName={"브랜치"} valueName={"dev-be"} value={selectedBackend.branch}/>
-            <DescBox desc={"해당 프레임워크를 빌드 시킬 브랜치명을 작성하세요"} /> */}
+        <InputBox
+          keyName={"폴더 경로"}
+          valueName={"/backend"}
+          value={selectedBackend.path}
+          onChange={changePathHandler}
+        />
+        <DescBox
+          desc={"프로젝트 루트 경로로부터 해당 프레임워크 폴더 경로를 작성하세요"}
+        />
 
-      <InputBox
-        keyName={"내부 포트 번호"}
-        valueName={"8080"}
-        value={selectedBackend.internalPort}
-        onChange={changeInternalPortHandler}
-      />
-      <DescBox desc={"해당 프레임워크가 사용할 포트 번호를 지정해주세요"} />
+        {/* <InputBox keyName={"브랜치"} valueName={"dev-be"} value={selectedBackend.branch}/>
+              <DescBox desc={"해당 프레임워크를 빌드 시킬 브랜치명을 작성하세요"} /> */}
 
-      {/* <InputBox keyName={"외부 포트 번호"} valueName={"8080"} value={selectedBackend.externalPort}/>
-            <DescBox desc={"해당 프레임워크가 사용할 포트 번호를 지정해주세요"} /> */}
+        <InputBox
+          keyName={"내부 포트 번호"}
+          valueName={"8080"}
+          value={selectedBackend.internalPort}
+          onChange={changeInternalPortHandler}
+        />
+        <DescBox desc={"해당 프레임워크가 사용할 포트 번호를 지정해주세요"} />
+
+        {/* <InputBox keyName={"외부 포트 번호"} valueName={"8080"} value={selectedBackend.externalPort}/>
+              <DescBox desc={"해당 프레임워크가 사용할 포트 번호를 지정해주세요"} /> */}
+      </div>
     </div>
   );
 }
