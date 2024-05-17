ab0e3bfb21cc956e04f68036d71c62055ca4dd8e
박헤선 <qkr8364@naver.com>
2024-05-17 17:31:09 +0900

    FEAT: 로그보기 새로고침

--- frontend/src/components/modal/LogModal.jsx
+++ frontend/src/components/modal/LogModal.jsx
@@ -1,13 +1,34 @@
 import { useRef } from "react";
 
+import toast from "react-hot-toast";
 import styles from "./LogModal.module.css";
 import close from "../../assets/close2.png";
+import reload from "../../assets/reload.png";
 
 import useModalStore from "../../stores/modalStore";
+import { getLog } from "../../api/Docker";
 
-export default function LogMadal({ content }) {
+export default function LogMadal({ serviceId }) {
   const modalBackground = useRef();
-  const { setLogModalOpen, logContent } = useModalStore();
+  const { setLogModalOpen, logContent, setLogContent } = useModalStore();
+
+  const handleLogModal = async (serviceId) => {
+    try {
+      const response = await getLog(serviceId);
+      if (response.status === 200) {
+        setLogContent(response.data);
+        console.log("로그재조회");
+        console.log(serviceId);
+        // console.log(response.data);
+      } else {
+        toast.error(`로그 조회 실패`, {
+          position: "top-center",
+        });
+      }
+    } catch (error) {
+      console.log("로그 조회 실패: " + error);
+    }
+  };
 
   return (
     <>
@@ -24,12 +45,22 @@ export default function LogMadal({ content }) {
           <div className={styles.modalhead}>
             <div
               className={styles.closeImg}
+              onClick={() => handleLogModal(serviceId)}
+            >
+              <img
+                src={reload}
+                alt=""
+                decoding="async"
+                className={styles.btnIcon}
+              />
+            </div>
+            <div
+              className={styles.closeImg}
               onClick={() => setLogModalOpen(false)}
             >
               <img
                 src={close}
                 alt=""
-                height="20px"
                 decoding="async"
                 className={styles.btnIcon}
               />
